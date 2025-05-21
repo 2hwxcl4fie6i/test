18fe0af2b3de8bf1c5daf9a3b3ff99f2c4c958e7
2hwxcl4fie6i <2hwxcl4fie6i@mail.ru>
2025-05-21 20:01:04 +0300

    Fix webpack publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         clean: true,
-        publicPath: '/test/'
+        publicPath: './'
     },
     module: {
         rules: [
